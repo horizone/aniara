5bd99aa7950595912686c9d92f334f1166f51659
unknown <perra.sjidin5@hotmail.com>
2017-09-22 00:11:49 +0200

    Added Auth

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,16 @@
 
 Route::get('/', function () {
     return view('welcome');
+
+Route::get('/login', 'App\Http\Controllers\Auth\LoginController');
+
+Route::get('/register', 'App\Http\Controllers\Auth\RegisterController');
+
+Route::get('/reset', 'App\Http\Controllers\Auth\ResetPasswordController');
+
+Route::get('/forgotpass', 'App\Http\Controllers\Auth\ForgotPasswordController');
+
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
